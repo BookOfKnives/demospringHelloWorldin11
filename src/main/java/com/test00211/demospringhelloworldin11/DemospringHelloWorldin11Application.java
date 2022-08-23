8c8fef6709d6e0cd0d0b4aeaa2759b65845e1e44
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2022-08-23 12:09:11 +0200

    added retetion days:1

--- src/main/java/com/test00211/demospringhelloworldin11/DemospringHelloWorldin11Application.java
+++ src/main/java/com/test00211/demospringhelloworldin11/DemospringHelloWorldin11Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemospringHelloWorldin11Application {
-
+//2308 2022 jeg har lavet dether for at teste om det er java11, som serveren på azure er sat til.
     public static void main(String[] args) {
         SpringApplication.run(DemospringHelloWorldin11Application.class, args);
     }
