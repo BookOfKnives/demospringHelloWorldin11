ada8c5811018f1e0f711e06c95e4a4c655a7fdf5
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2022-08-24 15:27:09 +0200

    changed name in master___yaml from myfirst to helloworld

--- src/main/java/com/test00211/demospringhelloworldin11/controller/helloController.java
+++ src/main/java/com/test00211/demospringhelloworldin11/controller/helloController.java
@@ -2,7 +2,6 @@ package com.test00211.demospringhelloworldin11.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class helloController {
 
